cc2adbb1f10ca938b2a27888fba6157804838023
pgntgz <zhipengzhang679@gmail.com>
2026-04-07 19:34:38 +0800

    style: 更日记

--- src/data/diary.ts
+++ src/data/diary.ts
@@ -16,8 +16,28 @@ const diaryData: DiaryItem[] = [
 	{
 		id: 1,
 		content:
-			"为了酒馆买的5刀grok,结果发现grok除了输出色，在galqq聊人表现特别的棒，我真他妈怀疑老马偷推特的文爱记录练ai了。grok个人感觉逻辑能力是真的差，不过可能因为nsfw训练资料吧，情商表现巨好。希望开放nsfw的厂商越来越多",
+			"为了酒馆买的5刀grok,结果发现grok输出特别适合在galqq，聊人表现特别的棒，我真他妈怀疑老马偷推特的文爱记录练ai了。grok个人感觉逻辑能力是真的差，不过可能因为开放nsfw训练资料吧，情商和淫商表现巨好。希望开放nsfw的厂商越来越多",
 		date: "2026-04-1T20:51:00Z",
+		images: [],
+	},
+];
+
+const diaryData: DiaryItem[] = [
+	{
+		id: 2,
+		content:
+			"妈的Mundfish会不会做机制？没有人喜欢你那个沟槽的响指锁，还有有效提高游戏难度系数的贪吃蛇。",
+		date: "2026-04-6T15:05:00Z",
+		images: [],
+	},
+];
+
+const diaryData: DiaryItem[] = [
+	{
+		id: 3,
+		content:
+			"奇异搞笑了，搞不懂byd DC为啥要是双击自动回应上次使用的表情，这才后知后觉的发现自己因为沟槽的误触给一堆无关内容回复了🇹🇼",
+		date: "2026-04-7T19:14:00Z",
 		images: [],
 	},
 ];
